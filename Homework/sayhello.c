2536ac0bdcc19daffe324068fd4275166e97a89f
jiacelyne <jiacelyne@icloud.com>
2025-08-27 15:13:48 -0700

    adding say hello c file

---
+++ Homework/sayhello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char * argv[] ) {
+
+    char input[25];
+    printf( "what is your name: ");
+    scanf( "%s", input);
+    printf( "\n Hello, %s\n", input );
+    exit( 0 );
+
+
+}
+
+
+
+
+    
+    
+
