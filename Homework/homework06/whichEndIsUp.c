cd0fc7b45b8e3615a67dda7149aeb86a37035a78
Cindy Tong <pikachut@Cindys-MacBook-Air.local>
2025-12-02 16:36:11 -0800

    Finished problem 4

---
+++ Homework/homework06/whichEndIsUp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void checkEndianness(uint32_t value) {
+    uint8_t *byte_ptr = (uint8_t *) &value;
+    printf("Value: 0x%08X\n", value);
+    printf("First byte in memory: 0x%02X\n", byte_ptr[0]);
+    if (byte_ptr[0] == (value >> 24)) {
+        printf("BIG ENDIAN\n");
+
+    } else if (byte_ptr[0] == (value & 0xFF)) {
+        printf("LITTLE ENDIAN\n");
+    }
+}
+
+int main() {
+    u_int16_t test_value;
+    printf("Enter a hex value (e.g., 0x01020304): ");
+    scanf("%x", &test_value);
+    
+    checkEndianness(test_value);
+    
+    return 0;
+
+}
